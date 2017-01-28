03985a1216cbc14ee9ad658c68d903855daf0a94
Michael Loster <michael.loster@hpi.de>
2017-01-28 22:49:06 +0100

    - Restructure the Layout. - Remove SideBar component and use Drawer component directly. - Add first small media query section to layout.css

--- src/components/Sidebar/sidebar.spec.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Sidebar from './sidebar'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-
-test('Sidebar snapshot test', () =>{
-  const component = shallow(<MuiThemeProvider><Sidebar>Testo</Sidebar></MuiThemeProvider>);
-  expect(component).toMatchSnapshot()
-});
