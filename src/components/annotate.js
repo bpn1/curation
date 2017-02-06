093ab0a1dcb0ef58f49262094fa55f6a1fdc7a57
Nils Strelow <nils.strelow@student.hpi.de>
2017-02-06 19:50:23 +0100

    move FloatingActionButton to bottom right

--- src/components/annotate.js
+++ src/components/annotate.js
@@ -14,10 +14,19 @@ class ContentCard extends Component{
       transition: 'all .45s ease-out'
     };
 
+    const fabStyle = {
+      margin: 0,
+      top: 'auto',
+      right: 20,
+      bottom: 20,
+      left: 'auto',
+      position: 'fixed',
+    };
+
     return (
       <Card zDepth={1} style={styles}>
-        <CardActions>
-          <FloatingActionButton>
+        <CardActions >
+          <FloatingActionButton style={fabStyle}>
             <ContentAdd />
           </FloatingActionButton>
         </CardActions>
