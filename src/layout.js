fe9e551b39fc9130766cbd73ac3e6a679fdd26e3
slayerpart <marvin.gorecki@student.hpi.uni-potsdam.de>
2017-02-14 16:10:54 +0100

    fix js linting in layout.js

--- src/layout.js
+++ src/layout.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { deepOrange500 } from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Avatar from 'material-ui/Avatar';
 
 import styles from './layout.css';
-import InteractiveTable from './components/interactive_table'
+import InteractiveTable from './components/interactive_table';
 import HeadBar from './components/HeadBar';
 import ToolBar from './containers/ToolBar';
 import SideBar from './containers/SideBar';
@@ -18,7 +18,7 @@ const theme = getMuiTheme({
     accent1Color: deepOrange500
   }
 });
-//const theme = getMuiTheme(darkBaseTheme);
+// const theme = getMuiTheme(darkBaseTheme);
 
 export const layoutBreakpoint = '(min-width: 769px)';
 
@@ -29,34 +29,30 @@ const appTitle = (
 );
 
 class Layout extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
     return (
       <MuiThemeProvider muiTheme={theme}>
         <div className={styles.appLayout}>
-            <HeadBar
-              showMiddle={false}
-              hasSearchBar={false}
-              right={<Avatar size={40} src={image} />}
-              left={appTitle}
-            />
+          <HeadBar
+            showMiddle={false}
+            hasSearchBar={false}
+            right={<Avatar size={40} src={image} />}
+            left={appTitle}
+          />
           <ToolBar />
           <section className={styles.appMainContainer}>
             <SideBar />
             <ContentCard>
               <InteractiveTable
                 headers={[
-                {key: "id", name: "ID"},
-                {key: "name", name: "Name"},
-                {key: "importantNumber", name: "Important Number"}
+                { key: 'id', name: 'ID' },
+                { key: 'name', name: 'Name' },
+                { key: 'importantNumber', name: 'Important Number' }
                 ]} data={[
-                {"id": 1, "name": "Test", "importantNumber": 1337},
-                {"id": 2, "name": "Testerino", "importantNumber": 42},
-                {"id": 3, "name": "Testung", "importantNumber": 18},
-                {"id": 1337, "name": "Testasterous", "importantNumber": 10000}
+                { id: 1, name: 'Test', importantNumber: 1337 },
+                { id: 2, name: 'Testerino', importantNumber: 42 },
+                { id: 3, name: 'Testung', importantNumber: 18 },
+                { id: 1337, name: 'Testasterous', importantNumber: 10000 }
                 ]}
               />
             </ContentCard>
