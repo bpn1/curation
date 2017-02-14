550e913a0eb0ccbe963e425cec619ec2ac33a0c1
slayerpart <marvin.gorecki@student.hpi.uni-potsdam.de>
2017-02-14 16:16:02 +0100

    fix toolbar linting - add proptypes validation

--- src/containers/ToolBar/index.js
+++ src/containers/ToolBar/index.js
@@ -1,42 +1,48 @@
-import React, {Component} from 'react';
-import styles from './toolbar.css';
+import React, { Component } from 'react';
 import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
+import { bindActionCreators } from 'redux';
 import IconButton from 'material-ui/IconButton';
 import Paper from 'material-ui/Paper';
 import MenuIcon from 'material-ui/svg-icons/navigation/menu';
 import { connect } from 'react-redux';
+
+import styles from './toolbar.css';
 import { toggleSideNav } from '../../actions/index';
-import { bindActionCreators } from 'redux';
 
 class ToolBar extends Component {
-    render() {
-        return (
-        <Paper zDepth={1} className={styles.appToolbarContainer}>
-          <Toolbar
-            className={styles.toolbar}
-            style={{backgroundColor: '#FAFAFA', padding: '3px', height: '53px'}}
-          >
-            <ToolbarGroup>
-              <IconButton>
-                <MenuIcon
-                  className={this.props.showSideNav? styles.rotate : ''}
-                  onTouchTap={() => this.props.toggleSideNav()}
-                />
-              </IconButton>
-            </ToolbarGroup>
-          </Toolbar>
-        </Paper>);
-    }
+  render() {
+    return (
+      <Paper zDepth={1} className={styles.appToolbarContainer}>
+        <Toolbar
+          className={styles.toolbar}
+          style={{ backgroundColor: '#FAFAFA', padding: '3px', height: '53px' }}
+        >
+          <ToolbarGroup>
+            <IconButton>
+              <MenuIcon
+                className={this.props.showSideNav ? styles.rotate : ''}
+                onTouchTap={() => this.props.toggleSideNav()}
+              />
+            </IconButton>
+          </ToolbarGroup>
+        </Toolbar>
+      </Paper>);
+  }
 }
 
+ToolBar.propTypes = {
+  showSideNav: React.propTypes.boo.isRequired,
+  toggleSideNav: React.propTypes.func.isRequired
+};
+
 function mapStateToProps(state) {
-    return {
-        showSideNav: state.showSideNav
-    };
+  return {
+    showSideNav: state.showSideNav
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ toggleSideNav: toggleSideNav }, dispatch);
+  return bindActionCreators({ toggleSideNav: toggleSideNav }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ToolBar);
