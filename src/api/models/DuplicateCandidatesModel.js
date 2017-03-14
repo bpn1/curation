2e61184d92d3dd6a3175fe58caae115ee42ae81b
Nils Strelow <nils.strelow@student.hpi.de>
2017-03-14 14:47:32 +0100

    update duplicatecandidates model to match datalake

--- src/api/models/DuplicateCandidatesModel.js
+++ src/api/models/DuplicateCandidatesModel.js
@@ -1,3 +1,26 @@
-const subjectModel = require('./SubjectModel');
-subjectModel.table_name = 'duplicatecandidates';
-module.exports = subjectModel;
+
+module.exports = {
+  fields: {
+    id: {
+      type: 'uuid',
+      default: { $db_function: 'uuid()' }
+    },
+    duplicate_id: {
+      type: 'uuid'
+    },
+    duplicate_name: {
+      type: 'text'
+    },
+    duplicate_table: {
+      type: 'text'
+    },
+    subject_id: {
+      type: 'uuid'
+    },
+    subject_name: {
+      type: 'text'
+    },
+  },
+  key: ['id'],
+  table_name: 'duplicatecandidates'
+};
