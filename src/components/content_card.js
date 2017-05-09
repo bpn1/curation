3105b50f123eaf8e01e93a72d97b7286a35449af
Nils Strelow <nils.strelow@student.hpi.de>
2017-05-09 16:56:11 +0200

    :fire: Remove FloatingActionButton

--- src/components/content_card.js
+++ src/components/content_card.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import muiThemable from 'material-ui/styles/muiThemeable';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
-import { Card, CardText } from 'material-ui/Card';
+import {Card, CardText} from 'material-ui/Card';
 
 class ContentCard extends Component {
   render() {
@@ -14,23 +12,11 @@ class ContentCard extends Component {
       transition: 'all .45s ease-out'
     };
 
-    const fabStyle = {
-      margin: 0,
-      top: 'auto',
-      right: 20,
-      bottom: 20,
-      left: 'auto',
-      position: 'fixed',
-    };
-
     return (
       <Card zDepth={2} style={styles}>
         <CardText>
           {this.props.children}
         </CardText>
-        <FloatingActionButton style={fabStyle}>
-          <ContentAdd />
-        </FloatingActionButton>
       </Card>
     );
   }
