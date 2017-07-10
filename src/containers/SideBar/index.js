bec107dde52a37ff301d8fc7b9b080d2827ef437
Nils Strelow <nils.strelow@student.hpi.de>
2017-07-10 13:17:37 +0200

    :rotating_light: Linter fixes SideBar

--- src/containers/SideBar/index.js
+++ src/containers/SideBar/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import bindActionCreators from 'redux/es/bindActionCreators';
 import connect from 'react-redux/es/connect/connect';
-import muiThemeable from "material-ui/styles/muiThemeable";
+import muiThemeable from 'material-ui/styles/muiThemeable';
 import Avatar from 'material-ui/Avatar';
 import Drawer from 'material-ui/Drawer';
 import { List, ListItem, makeSelectable } from 'material-ui/List';
@@ -10,7 +10,6 @@ import Subheader from 'material-ui/Subheader';
 import Divider from 'material-ui/Divider';
 import ContentInbox from 'material-ui/svg-icons/content/inbox';
 import ContentLink from 'material-ui/svg-icons/content/link';
-import ActionInfo from 'material-ui/svg-icons/action/info';
 import ActionHistory from 'material-ui/svg-icons/action/history';
 import ActionAssessment from 'material-ui/svg-icons/action/assessment';
 import SocialGroup from 'material-ui/svg-icons/social/group';
@@ -21,9 +20,6 @@ import toggleSideNav from '../../actions/index';
 import styles from './sidebar.css';
 import { layoutBreakpoint } from '../../layout';
 import image from '../../../logo.png';
-
-import { commerzbankYellow } from '../../themes/curation';
-import { grey700 } from 'material-ui/styles/colors';
 
 // Function copied from http://www.material-ui.com/#/components/list: Selectable list
 function wrapState(ComposedComponent) {
@@ -74,7 +70,7 @@ class SideBar extends Component {
     };
     return (
       <div className={styles.sidebarWrapper}>
-        <div className={overlayClass} onClick={() => this.props.toggleSideNav()}/>
+        <div className={overlayClass} onClick={() => this.props.toggleSideNav()} />
         <Drawer
           docked
           open={this.props.showSideNav}
@@ -83,24 +79,66 @@ class SideBar extends Component {
           containerClassName={styles.sideNav}
           containerStyle={Object.assign(sideBarStyle, { backgroundColor: this.props.muiTheme.palette.backgroundColor })}
         >
-          { !window.matchMedia(layoutBreakpoint).matches &&
-            <div className={styles.avatarContainer}> <Avatar size={80} src={image} /> </div> }
+          {!window.matchMedia(layoutBreakpoint).matches &&
+          <div className={styles.avatarContainer}><Avatar size={80} src={image} /></div>}
           <SelectableList
-              defaultValue={1}
-              selectedItemStyle={selectedItemStyle}
-              onClick={window.matchMedia(layoutBreakpoint).matches ? () => {} : () => this.props.toggleSideNav()}>
-            <ListItem value={1} primaryText="Subjects" containerElement={<Link to={'/'} />} leftIcon={<ContentInbox />} />
-            <ListItem value={2} primaryText="Versions" containerElement={<Link to={'/versions'} />} leftIcon={<ActionHistory />} />
-            <ListItem value={3} primaryText="Graphs" containerElement={<Link to={'/graphs'} />} leftIcon={<GraphIcon />} />
+            defaultValue={1}
+            selectedItemStyle={selectedItemStyle}
+            onClick={window.matchMedia(layoutBreakpoint).matches ? () => {
+            } : () => this.props.toggleSideNav()}
+          >
+            <ListItem
+              value={1}
+              primaryText="Subjects"
+              containerElement={<Link to={'/'} />}
+              leftIcon={<ContentInbox />}
+            />
+            <ListItem
+              value={2}
+              primaryText="Versions"
+              containerElement={<Link to={'/versions'} />}
+              leftIcon={<ActionHistory />}
+            />
+            <ListItem
+              value={3}
+              primaryText="Graphs"
+              containerElement={<Link to={'/graphs'} />}
+              leftIcon={<GraphIcon />}
+            />
             <Subheader>Deduplication</Subheader>
             <Divider className={styles.sideDivider} />
-            <ListItem value={4} primaryText="Duplicates" containerElement={<Link to={'/duplicates'} />} leftIcon={<SocialGroup />} />
-            <ListItem value={5} primaryText="Blocking&nbsp;Statistics" containerElement={<Link to={'/statistics/blocking'} />} leftIcon={<ActionAssessment />} />
-            <ListItem value={6} primaryText="Similarity&nbsp;Measure" containerElement={<Link to={'/statistics/simmeasure'} />} leftIcon={<ActionAssessment />} />
+            <ListItem
+              value={4}
+              primaryText="Duplicates"
+              containerElement={<Link to={'/duplicates'} />}
+              leftIcon={<SocialGroup />}
+            />
+            <ListItem
+              value={5}
+              primaryText="Blocking&nbsp;Statistics"
+              containerElement={<Link to={'/statistics/blocking'} />}
+              leftIcon={<ActionAssessment />}
+            />
+            <ListItem
+              value={6}
+              primaryText="Similarity&nbsp;Measure"
+              containerElement={<Link to={'/statistics/simmeasure'} />}
+              leftIcon={<ActionAssessment />}
+            />
             <Subheader>Textmining</Subheader>
             <Divider className={styles.sideDivider} />
-            <ListItem value={7} primaryText="Entity Linking" containerElement={<Link to={'/entity_linking'} />} leftIcon={<ContentLink />} />
-            <ListItem value={8} primaryText="Classifier&nbsp;Statistics" containerElement={<Link to={'/statistics/classifier'} />} leftIcon={<ActionAssessment />} />
+            <ListItem
+              value={7}
+              primaryText="Entity Linking"
+              containerElement={<Link to={'/entity_linking'} />}
+              leftIcon={<ContentLink />}
+            />
+            <ListItem
+              value={8}
+              primaryText="Classifier&nbsp;Statistics"
+              containerElement={<Link to={'/statistics/classifier'} />}
+              leftIcon={<ActionAssessment />}
+            />
           </SelectableList>
         </Drawer>
       </div>
